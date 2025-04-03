9ac0c3ee5293ba1ec3b965d3cdd64de825f9b375
Abdul Basit <abdulbasitbasit076@gmail.com>
2025-04-03 16:34:09 +0500

    Fixed dependencies

--- config/db.js
+++ config/db.js
@@ -1,11 +1,8 @@
-import mongoose from"mongoose";
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI); 
     console.log("MongoDB connected successfully.");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
@@ -13,4 +10,4 @@ const connectDB = async () => {
   }
 };
 
-export { connectDB}
+export { connectDB };
